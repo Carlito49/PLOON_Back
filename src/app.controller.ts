2508bf2c1f412d7149b50923da57a1023a9b4a47
Carlit49 <charlesquellin@hotmail.com>
2022-03-02 10:57:58 +0100

    push 02/03 recommencer les modules utilisateurs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
-import { Utilisateur } from 'entities/entities/Utilisateur';
 import { AppService } from './app.service';
-import { UtilisateurService } from './user/utilisateur.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService, private readonly utilisateurService: UtilisateurService) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getUtilisateurs(): string {
       return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { typeOrmConfig } from './config/typeorm.config';
-import { UtilisateurModule } from './user/utilisateur.module';
+
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), UtilisateurModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
