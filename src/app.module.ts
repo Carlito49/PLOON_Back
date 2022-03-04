93432076f70db900f5c6d73d4e88a339309e81e6
Carlit49 <charlesquellin@hotmail.com>
2022-03-04 12:30:10 +0100

    push v1 04/03 (retour au départ, trop de soucis avec typeOrm)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,34 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { typeOrmConfig } from './config/typeorm.config';
-import { CentreInteretsRepository } from './modules/centreinterets/centreInterets.repository';
-import { CentreInteretsService } from './modules/centreinterets/centreinterets.service';
-import { LieuRencontreModule } from './modules/lieuRencontre/lieuRencontre.module';
-import { LieuRencontreRepository } from './modules/lieuRencontre/lieuRencontre.repository';
-import { LieuRencontreService } from './modules/lieuRencontre/lieuRencontre.service';
-import { UserModule } from './modules/user/user.module';
-import { UserRepository } from './modules/user/user.repository';
-import { UserService } from './modules/user/user.service';
-import { UtilisateurCentreInteretsModule } from './modules/utilisateurCentreInterets/utilisateurCentreInterets.module';
-import { UtilisateurCentreInteretsRepository } from './modules/utilisateurCentreInterets/utilisateurCentreInterets.repository';
-import { UtilisateurCentreInteretsService } from './modules/utilisateurCentreInterets/utilisateurCentreInterets.service';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), 
-    UserModule, 
-    LieuRencontreModule,
-    UtilisateurCentreInteretsModule
+  imports: [TypeOrmModule.forRoot(typeOrmConfig),
   ],
   controllers: [AppController],
   providers: [AppService, 
-    UserService, 
-    UserRepository,
-    LieuRencontreService,
-    LieuRencontreRepository,
-    CentreInteretsService,
-    CentreInteretsRepository,
-    UtilisateurCentreInteretsService,
-    UtilisateurCentreInteretsRepository
   ],
 })
 export class AppModule {}
