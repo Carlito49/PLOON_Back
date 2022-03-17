cb7d6b502b7d27e3ff7463dd33a1cb3d03a987a3
Carlito49 <charlesquellin@hotmail.com>
2022-03-17 12:08:07 +0100

    ajout des headers? accès aux données

--- src/utilisateur/utilisateur.controller.ts
+++ src/utilisateur/utilisateur.controller.ts
@@ -2,12 +2,24 @@ import { Controller, Get, Param } from '@nestjs/common';
 import { Utilisateur } from 'output/entities/Utilisateur';
 import { UtilisateurService } from './utilisateur.service';
 
-@Controller('utilisateur')
+@Controller('user')
 export class UtilisateurController 
 {
-    constructor(private readonly utilisateurService: UtilisateurService) {}
+    constructor(private readonly utilisateurService: UtilisateurService) 
+    {
+      let headers = new Headers();
 
+      headers.append('Access-Control-Allow-Origin', 'http://localhost:19006');
+      headers.append('Access-Control-Allow-Credentials', 'true');
+    }
+  
     @Get()
+    getUser(): string {
+
+      return 'Hello tu es bien sur le controller user!!';
+    }
+
+    @Get('find')
     getUtilisateurs(): Promise<Utilisateur[]> {
         return this.utilisateurService.findAll();
     }
