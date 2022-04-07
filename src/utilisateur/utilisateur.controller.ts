1945d63e64c0f07aa6f1b10c35cea7f72e5d421b
Carlito49 <charlesquellin@hotmail.com>
2022-04-07 09:56:38 +0200

    1er push 07/04

--- src/utilisateur/utilisateur.controller.ts
+++ src/utilisateur/utilisateur.controller.ts
@@ -1,21 +1,16 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Header, Param } from '@nestjs/common';
 import { Utilisateur } from 'output/entities/Utilisateur';
 import { UtilisateurService } from './utilisateur.service';
 
 @Controller('user')
 export class UtilisateurController 
 {
-    constructor(private readonly utilisateurService: UtilisateurService) 
-    {
-      let headers = new Headers();
-
-      headers.append('Access-Control-Allow-Origin', 'http://localhost:19006');
-      headers.append('Access-Control-Allow-Credentials', 'true');
-    }
+    constructor(private readonly utilisateurService: UtilisateurService) {}
   
     @Get()
+    @Header('Access-Control-Allow-Origin', 'http://localhost:19006')
+    @Header('Access-Control-Allow-Credentials', 'true')
     getUser(): string {
-
       return 'Hello tu es bien sur le controller user!!';
     }
 
